5fee78d901b42f36daa95d4c06125ae76d6ce7fb
raffi1 <rafaelluiscueva545@gmail.com>
2023-03-13 00:54:47 +0800

    Cueva_calcv1

--- app/src/main/java/com/example/cueva_firstcalculatorv1/MainActivity.java
+++ app/src/main/java/com/example/cueva_firstcalculatorv1/MainActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.InputText);
+        display.setShowSoftInputOnFocus(false);
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getString(R.string.display).equals(display.getText().toString().toString())){
+                    display.setText("");
+                }
+            }
+        });
+
     }
 
     public void clearBTN (View view){
@@ -53,14 +68,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void sixBTN (View view){
 
+    }
 
+    public void subtractBTN (View view){
 
+    }
 
+    public void oneBTN (View view){
 
+    }
 
+    public void twoBTN (View view){
 
+    }
 
+    public void threeBTN (View view){
+
+    }
+
+    public void plusBTN (View view){
+
+    }
+
+    public void plusMinusBTN (View view){
+
+    }
+
+    public void zeroBTN (View view){
+
+    }
+
+    public void pointBTN (View view){
+
+    }
+
+    public void equalBTN (View view){
+
+    }
+
+    public void backspaceBTN (View view){
+
+    }
 
 
 }
