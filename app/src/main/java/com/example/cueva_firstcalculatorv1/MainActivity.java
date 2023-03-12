a1ee046ea824e1457043d19f08ec024f380a4ffd
raffi1 <rafaelluiscueva545@gmail.com>
2023-03-13 00:41:13 +0800

    Cueva_calcv1

--- app/src/main/java/com/example/cueva_firstcalculatorv1/MainActivity.java
+++ app/src/main/java/com/example/cueva_firstcalculatorv1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cueva_firstcalculatorv1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clearBTN (View view){
+
+    }
+
+    public void parenthesesBTN (View view){
+
+    }
+
+    public void exponentBTN (View view){
+
+    }
+
+    public void divideBTN (View view){
+
+    }
+
+    public void sevenBTN (View view){
+
+    }
+
+    public void eightBTN (View view){
+
+    }
+
+    public void nineBTN (View view){
+
+    }
+
+    public void multiplyBTN (View view){
+
+    }
+
+    public void fourBTN (View view){
+
+    }
+
+    public void fiveBTN (View view){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
